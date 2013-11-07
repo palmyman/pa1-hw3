c4a56911837574b5a9229a382199f8999d6c9b7c
Lubomír Čuhel <cuhel.lubomir@gmail.com>
2013-11-07 14:07:02 +0100

    init

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: palmyman
+ *
+ * Created on November 7, 2013, 2:03 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
