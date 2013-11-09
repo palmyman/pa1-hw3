765e50bcb5292f8af040df10d0ac8d1d5184d797
Lubomír Čuhel <cuhel.lubomir@gmail.com>
2013-11-09 16:43:14 +0100

    slow version done

--- main.c
+++ main.c
@@ -70,8 +70,8 @@ int dateIsValid(const TDATETIME * date) {
     if (date->y < 1600 ||
             date->m < 1 ||
             date->d < 1 ||
-            date->h < 1 ||
-            date->i < 1 ||
+            date->h < 0 ||
+            date->i < 0 ||
             date->m > 12 ||
             date->d > daysInMonth(date->m, date->y) ||
             date->h > 23 ||
@@ -90,97 +90,172 @@ int dayNumber(const TDATETIME * date) {
     return number;
 }
 
-int leapYearsInInterval(const TDATETIME * date1, const TDATETIME * date2) {
+int leapYearsInInterval(const TDATETIME * date) {
     long long int count1 = 0, count2 = 0, count3 = 0, limit, start, blockCount;
     int count = 0;
     int i;
 
-    if (date2->y - date1->y < 4000) {
-        for (i = date1->y; i < date2->y; i++) {
+    if (date->y - 1600 < 4000) {
+        for (i = 1600; i < date->y; i++) {
             count += isLeapYear(i);
         }
         return count;
     }
 
-    limit = ((date1->y / 4000) + 1)*4000;
-    start = ((date2->y / 4000) - 1)*4000;
+    limit = ((1600 / 4000) + 1)*4000;
+    start = ((date->y / 4000) - 1)*4000;
 
-    for (i = date1->y; i < limit; i++) {
+    for (i = 1600; i < limit; i++) {
         count1 += isLeapYear(i);
     }
 
     blockCount = start / 4000 - limit / 4000;
     count2 = blockCount * 969;
 
-    for (i = start; i < date2->y; i++) {
+    for (i = start; i < date->y; i++) {
         count3 += isLeapYear(i);
     }
 
     return count1 + count2 + count3;
 }
 
-int comparator(const TDATETIME * date1, const TDATETIME * date2) {
-    if (date1->y != date2->y) return (date1->y - date2->y);
-    if (date1->m != date2->m) return (date1->m - date2->m);
+int comparator(const TDAYTIME * date1, const TDAYTIME * date2) {
     if (date1->d != date2->d) return (date1->d - date2->d);
     if (date1->h != date2->h) return (date1->h - date2->h);
     return (date1->i - date2->i);
 }
 
 TDAYTIME convertForm(const TDATETIME * date) {
-    TDAYTIME newDate;
-    newDate.d = (date->y - 1600) * 365 + dayNumber(date) - 1 + leapYearsInInterval(1600, date);
-    newDate.h = date->h;
-    newDate.i = date->i;
-    return newDate;
+    TDAYTIME time;
+    time.d = (date->y - 1600) * 365 + dayNumber(date) + leapYearsInInterval(date);
+    time.h = date->h;
+    time.i = date->i;
+    return time;
 }
 
-void incDate(TDAYTIME * date) {
-    if(date->i < 59) {
-        date->i++;
-    } else if (date->h < 23) {
-        date->i = 0;
-        date->h++;
+void incTime(TDAYTIME * time) {
+    if (time->i < 59) {
+        time->i++;
+    } else if (time->h < 23) {
+        time->i = 0;
+        time->h++;
     } else {
-        date->i = 0;
-        date->h = 0;
-        date->d++;
+        time->i = 0;
+        time->h = 0;
+        time->d++;
     }
 }
 
-int isCockooTime(const TDATETIME * date) {
-    if (date->m == 30) return 1;
-    if (date->m == 0) return date->h;
+int isCuckooTime(const TDAYTIME * time) {
+    if (time->i == 30) return 1;
+    if (time->i == 0) {
+        if (time->h == 0) return 12;
+        if (time->h > 12) return (time->h - 12);
+        return time->h;
+    }
     return 0;
 }
 
 int cuckooClock(int y1, int m1, int d1, int h1, int i1,
         int y2, int m2, int d2, int h2, int i2,
         long long int * cuckoo) {
-    TDATETIME * date1, * date2;
-    date1->y = y1;
-    date1->m = m1;
-    date1->d = d1;
-    date1->h = h1;
-    date1->i = i1;
+    TDATETIME date1, date2;
 
-    date2->y = y2;
-    date2->m = m2;
-    date2->d = d2;
-    date2->h = h2;
-    date2->i = i2;
+    date1.y = y1;
+    date1.m = m1;
+    date1.d = d1;
+    date1.h = h1;
+    date1.i = i1;
 
-    if (!dateIsValid(date1) || !dateIsValid(date2) || comparator(date1, date2) <= 0)
+    date2.y = y2;
+    date2.m = m2;
+    date2.d = d2;
+    date2.h = h2;
+    date2.i = i2;
+
+    if (!dateIsValid(&date1) || !dateIsValid(&date2)) {
+        printf("Invalid date!!\n");
         return 0;
+    }
+
+    TDAYTIME time1, time2;
+    time1 = convertForm(&date1);
+    time2 = convertForm(&date2);
+
+    if (comparator(&time1, &time2) > 0) {
+        printf("Date1 later than date2!!\n");
+        return 0;
+    }
     
-    
+    * cuckoo = 0;
+    if (0) {
+        * cuckoo += ((time2.d - time1.d - 1) * 90);
+        time1.d = time2.d - 1;
+    }
+
+    while (comparator(&time1, &time2) <= 0) {
+        * cuckoo += isCuckooTime(&time1);
+        incTime(&time1);
+    }
+
+    printf("cuckoo = %lld\n", * cuckoo);
+
     return 1;
 }
 
 #ifndef __PROGTEST__
 
 int main(int argc, char** argv) {
+    long long int cuckoo;
+    int res;
 
+    res = cuckooClock(2013, 10, 1, 13, 15,
+            2013, 10, 1, 18, 45, &cuckoo);
+    /* res = 1, cuckoo = 26 */
+
+    res = cuckooClock(2013, 10, 1, 13, 15,
+            2013, 10, 2, 11, 20, &cuckoo);
+    /* res = 1, cuckoo = 165 */
+
+    res = cuckooClock(2013, 1, 1, 13, 15,
+            2013, 10, 5, 11, 20, &cuckoo);
+    /* res = 1, cuckoo = 49845 */
+
+    res = cuckooClock(2012, 1, 1, 13, 15,
+            2012, 10, 5, 11, 20, &cuckoo);
+    /* res = 1, cuckoo = 50025 */
+
+    res = cuckooClock(1900, 1, 1, 13, 15,
+            1900, 10, 5, 11, 20, &cuckoo);
+    /* res = 1, cuckoo = 49845 */
+
+    res = cuckooClock(2013, 10, 1, 0, 0,
+            2013, 10, 1, 12, 0, &cuckoo);
+    /* res = 1, cuckoo = 102 */
+
+    res = cuckooClock(2013, 10, 1, 0, 15,
+            2013, 10, 1, 0, 25, &cuckoo);
+    /* res = 1, cuckoo = 0 */
+
+    res = cuckooClock(2013, 10, 1, 12, 0,
+            2013, 10, 1, 12, 0, &cuckoo);
+    /* res = 1, cuckoo = 12 */
+
+    res = cuckooClock(2013, 11, 1, 12, 0,
+            2013, 10, 1, 12, 0, &cuckoo);
+    /* res = 0, cuckoo = N/A */
+
+    res = cuckooClock(2013, 10, 32, 12, 0,
+            2013, 11, 10, 12, 0, &cuckoo);
+    /* res = 0, cuckoo = N/A */
+
+    res = cuckooClock(2100, 2, 29, 12, 0,
+            2100, 2, 29, 12, 0, &cuckoo);
+    /* res = 0, cuckoo = N/A */
+
+    res = cuckooClock(2400, 2, 29, 12, 0,
+            2400, 2, 29, 12, 0, &cuckoo);
+    /* res = 1, cuckoo = 12 */
     return (EXIT_SUCCESS);
 }
 #endif /* __PROGTEST__ */
